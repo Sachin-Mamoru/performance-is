ba98090cbcda020d8606606828cfc2f7ba189bff
Sachin-Mamoru <sachinm@wso2.com>
2024-01-15 20:52:26 +0530

    improvement

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -125,15 +125,15 @@ declare -A test_scenario2=(
 #    [skip]=false
 #    [modes]="FULL QUICK"
 #)
-declare -A test_scenario12=(
-    [name]="12-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_groups_and_roles"
-    [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes Groups and Roles"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-    [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK PUBLISH OIDC_AUTH_CODE_REDIRECT_WITHOUT_CONSENT_UA_GROUPS_ROLES_FLOW"
-)
+#declare -A test_scenario12=(
+#    [name]="12-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_groups_and_roles"
+#    [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes Groups and Roles"
+#    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
+#    [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
+#    [tenantMode]=false
+#    [skip]=false
+#    [modes]="FULL QUICK PUBLISH OIDC_AUTH_CODE_REDIRECT_WITHOUT_CONSENT_UA_GROUPS_ROLES_FLOW"
+#)
 #declare -A test_scenario13=(
 #    [name]="13-Token_Exchange_Grant"
 #    [display_name]="Token Exchange Grant"
