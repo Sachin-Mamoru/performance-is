4f89e343004346bfbeb7b6d4137b99a689f99983
Sachin-Mamoru <sachinm@wso2.com>
2025-02-18 20:18:17 +0530

    test

--- common/jmeter/perf-test-is.sh
+++ common/jmeter/perf-test-is.sh
@@ -248,7 +248,7 @@ heap_regex='^[0-9]+[MG]$'
 # Check concurrency level
 if [ "$concurrency" == "50-500" ]; then
     echo "Running tests for concurrency level 50-500"
-    default_concurrent_users="50 100 150 300 500"
+    default_concurrent_users="150 300 500"
 elif [ "$concurrency" == "500-3000" ]; then
     echo "Running tests for concurrency level 500-3000"
     default_concurrent_users="500 1000 1500 2000 2500 3000"
@@ -532,7 +532,7 @@ function initiailize_test() {
             done
         done
     fi
-    
+
     if [[ ! -z $mode ]]; then
         declare -n scenario
         for scenario in ${!test_scenario@}; do
