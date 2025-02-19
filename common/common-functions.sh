9e3eec145bac15e245cf7d6c8f786ebd7fa47c31
Sachin Mamoru <59449070+Sachin-Mamoru@users.noreply.github.com>
2025-02-19 09:32:36 +0530

    Update common-functions.sh

--- common/common-functions.sh
+++ common/common-functions.sh
@@ -65,13 +65,13 @@ function exit_handler() {
     cat "$stack_events_json" | jq '.StackEvents | .[] | select ( .ResourceStatus == "CREATE_FAILED" )'
 
     local stack_delete_start_time=$(date +%s)
-    echo ""
-    echo "Deleting the stack: $stack_id"
-    aws cloudformation delete-stack --stack-name "$stack_id"
+    # echo ""
+    # echo "Deleting the stack: $stack_id"
+    # aws cloudformation delete-stack --stack-name "$stack_id"
 
-    echo ""
-    echo "Polling till the stack deletion completes..."
-    aws cloudformation wait stack-delete-complete --stack-name "$stack_id"
+    # echo ""
+    # echo "Polling till the stack deletion completes..."
+    # aws cloudformation wait stack-delete-complete --stack-name "$stack_id"
     printf "Stack deletion time: %s\n" "$(format_time $(measure_time "$stack_delete_start_time"))"
 
     printf "Script execution time: %s\n" "$(format_time $(measure_time "$script_start_time"))"
