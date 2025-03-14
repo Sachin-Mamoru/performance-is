1dbd25a5785ff84b77ff228dcbe8bf61229b6b5e
Sachin-Mamoru <sachinm@wso2.com>
2025-03-14 11:15:50 +0530

    test

--- common/deployment/setup/update-is-conf.sh
+++ common/deployment/setup/update-is-conf.sh
@@ -226,7 +226,7 @@ sed -i "s|{keystore_type}|$keystore_type|g" \
 sed -i "s|{is_case_insensitive_username_and_attributes}|$is_case_insensitive_username_and_attributes|g" \
   "$carbon_home"/repository/conf/deployment.toml || echo "Editing deployment.toml file failed!"
 
-sed -i 's|securerandom.source=file:/dev/random|securerandom.source=file:/dev/urandom|' $JAVA_HOME/conf/security/java.security
+sudo sed -i 's|securerandom.source=file:/dev/random|securerandom.source=file:/dev/urandom|' $JAVA_HOME/conf/security/java.security
 
 if [[ $db_type == "mysql" ]]; then
     update_mysql_config
