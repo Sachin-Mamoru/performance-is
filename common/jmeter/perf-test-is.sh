78c7146e7284f54637697572dc8c41744091be5a
Sachin-Mamoru <sachinm@wso2.com>
2025-04-16 21:24:02 +0530

    test

--- common/jmeter/perf-test-is.sh
+++ common/jmeter/perf-test-is.sh
@@ -260,7 +260,7 @@ elif [ "$concurrency" == "50-50" ]; then
     default_concurrent_users="50"
 else
     echo "Running tests for concurrency level 50-3000"
-    default_concurrent_users="50 100 150 300 500 1000 1500 2000"
+    default_concurrent_users="50 100 150 300 500 1000 1500 2000 2500 3000"
 fi
 
 if [[ -z $test_duration ]]; then
