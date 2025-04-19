173b1bc0dabfa4a2db36d85c638fd2c376a484cc
Sachin-Mamoru <sachinm@wso2.com>
2025-04-19 15:09:16 +0530

    test

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -24,7 +24,7 @@ declare -A test_scenario0=(
     [jmx]="oauth/OAuth_Client_Credentials_Grant.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL PUBLISH2"
+    [modes]="PUBLISH"
 )
 declare -A test_scenario1=(
     [name]="01-oidc_auth_code_redirect_with_consent"
@@ -33,34 +33,7 @@ declare -A test_scenario1=(
     [jmx]="oidc/OIDC_AuthCode_Redirect_WithConsent.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL QUICK PUBLISH2"
-)
-declare -A test_scenario2=(
-    [name]="02-oidc_auth_code_redirect_with_consent_retrieve_user_attributes"
-    [display_name]="OIDC Auth Code Grant Redirect With Consent Retrieve User Attributes"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-    [jmx]="oidc/OIDC_AuthCode_Redirect_WithConsent_Retrieve_User_Attributes.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK"
-)
-declare -A test_scenario03=(
-    [name]="03-oidc_auth_code_redirect_with_consent_retrieve_user_attributes_and_groups"
-    [display_name]="OIDC Auth Code Grant Redirect With Consent Retrieve User Attributes and Groups"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-    [jmx]="oidc/OIDC_AuthCode_Redirect_WithConsent_Retrieve_User_Attributes_And_Groups.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK"
-)
-declare -A test_scenario04=(
-    [name]="04-oidc_auth_code_redirect_with_consent_retrieve_user_attributes_groups_and_roles"
-    [display_name]="OIDC Auth Code Grant Redirect With Consent Retrieve User Attributes Groups and Roles"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-    [jmx]="oidc/OIDC_AuthCode_Redirect_WithConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK"
+    [modes]="QUICK PUBLISH"
 )
 declare -A test_scenario05=(
     [name]="05-oidc_auth_code_redirect_without_consent"
@@ -69,7 +42,7 @@ declare -A test_scenario05=(
     [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL QUICK PUBLISH2"
+    [modes]="QUICK PUBLISH"
 )
 declare -A test_scenario06=(
     [name]="06-oidc_auth_code_redirect_without_consent_retrieve_user_attributes"
@@ -79,15 +52,6 @@ declare -A test_scenario06=(
     [tenantMode]=false
     [skip]=false
     [modes]="FULL QUICK PUBLISH2"
-)
-declare -A test_scenario07=(
-    [name]="07-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_and_groups"
-    [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes and Groups"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-    [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_And_Groups.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK"
 )
 declare -A test_scenario08=(
     [name]="08-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_groups_and_roles"
@@ -107,33 +71,6 @@ declare -A test_scenario09=(
     [skip]=false
     [modes]="FULL QUICK PUBLISH2"
 )
-declare -A test_scenario10=(
-    [name]="10-oidc_password_grant_retrieve_user_attributes"
-    [display_name]="OIDC Password Grant Type Retrieve User Attributes"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
-    [jmx]="oidc/OIDC_Password_Grant_Retrieve_User_Attributes.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK"
-)
-declare -A test_scenario11=(
-    [name]="11-oidc_password_grant_retrieve_user_attributes_and_groups"
-    [display_name]="OIDC Password Grant Type Retrieve User Attributes and Groups"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
-    [jmx]="oidc/OIDC_Password_Grant_Retrieve_User_Attributes_And_Groups.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK"
-)
-declare -A test_scenario12=(
-    [name]="12-oidc_password_grant_retrieve_user_attributes_groups_and_roles"
-    [display_name]="OIDC Password Grant Type Retrieve User Attributes Groups and Roles"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
-    [jmx]="oidc/OIDC_Password_Grant_Retrieve_User_Attributes_Groups_And_Roles.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK"
-)
 declare -A test_scenario13=(
     [name]="13-saml2_sso_redirect_binding"
     [display_name]="SAML2 SSO Redirect Binding"
@@ -141,7 +78,7 @@ declare -A test_scenario13=(
     [jmx]="saml/SAML2_SSO_Redirect_Binding.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL QUICK PUBLISH"
+    [modes]="QUICK PUBLISH2"
 )
 declare -A test_scenario14=(
     [name]="14-Token_Exchange_Grant"
@@ -150,14 +87,5 @@ declare -A test_scenario14=(
     [jmx]="oauth/Token_Exchange_Grant.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL QUICK PUBLISH"
-)
-declare -A test_scenario15=(
-    [name]="15-B2B_oidc_auth_code_redirect_with_consent"
-    [display_name]="B2B OIDC Auth Code Grant Redirect With Consent"
-    [description]="Obtain an access token using the OAuth 2.0 authorization code grant type as a Sub org user to a Shared App."
-    [jmx]="oidc/B2B_OIDC_AuthCode_Redirect_WithConsent.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="B2B"
+    [modes]="QUICK PUBLISH2"
 )
