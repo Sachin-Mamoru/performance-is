3083536cfd2eaeff52003e7bb7b605fea6ffffe8
Sachin-Mamoru <sachinm@wso2.com>
2025-04-27 09:23:13 +0530

    test

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -24,7 +24,7 @@ declare -A test_scenario0=(
     [jmx]="oauth/OAuth_Client_Credentials_Grant.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL2 PUBLISH2"
+    [modes]="FULL2 PUBLISH"
 )
 declare -A test_scenario1=(
     [name]="01-oidc_auth_code_redirect_with_consent"
