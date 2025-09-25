778069c2e33ffbb7f04d29c0af7280ec8aaf6a75
Sachin Mamoru <59449070+Sachin-Mamoru@users.noreply.github.com>
2025-09-25 14:12:16 +0530

    update

--- common/jmeter/perf-test-is.sh
+++ common/jmeter/perf-test-is.sh
@@ -689,7 +689,7 @@ function test_scenarios() {
                 if [ "$SHOULD_RUN" = true ]; then
 
                     # Set your target time in UTC or your system's local time
-                    TARGET_TIME="2025-09-25 06:24:00"
+                    TARGET_TIME="2025-09-25 09:42:00"
 
                     # Convert the target time to epoch timestamp
                     TARGET_EPOCH=$(date -d "$TARGET_TIME" +%s)
