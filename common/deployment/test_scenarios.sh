e7acebc91ff65d6ccf37f63ff6486d1aa8459770
Sachin Mamoru <59449070+Sachin-Mamoru@users.noreply.github.com>
2025-10-01 16:22:14 +0530

    update

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -69,7 +69,7 @@ declare -A test_scenario05=(
     [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL QUICK PUBLISH"
+    [modes]="FULL QUICK PUBLISH2"
 )
 declare -A test_scenario06=(
     [name]="06-oidc_auth_code_redirect_without_consent_retrieve_user_attributes"
@@ -78,7 +78,7 @@ declare -A test_scenario06=(
     [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL QUICK PUBLISH2"
+    [modes]="FULL QUICK PUBLISH"
 )
 declare -A test_scenario07=(
     [name]="07-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_and_groups"
