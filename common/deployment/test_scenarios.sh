1dca8e30d00171293d1eb1552a26a0ff9aafeec6
Sachin Mamoru <59449070+Sachin-Mamoru@users.noreply.github.com>
2025-11-09 10:12:49 -0800

    update

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -24,7 +24,7 @@ declare -A test_scenario0=(
     [jmx]="oauth/OAuth_Client_Credentials_Grant.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL PUBLISH22"
+    [modes]="FULL PUBLISH2"
 )
 declare -A test_scenario1=(
     [name]="01-oidc_auth_code_redirect_with_consent"
